27346abce01d45a0c6352589303aa12fefb90c7d
kianvdb <kian.van.den.bussche@student.ehb.be>
2024-12-22 14:31:54 +0100

    Add Unity events to interaction

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
- 
+   //Add or remove an InteractionEvent component to this gameObject.
+ public bool useEvents;
+ [SerializeField]
  public string promptMessage;
+
+
+public virtual string Onlook(){
+   return promptMessage;
+}
+
+
 //This function will be called from our player.
  public void BaseInteract(){
+   if (useEvents)
+   GetComponent<InteractionEvent>().OnInteract.Invoke();
     Interact();
  }
 
