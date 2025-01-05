0eb9277599ecfddc58ce2b8334dd1a68617e2ea0
kianvdb <kian.van.den.bussche@student.ehb.be>
2025-01-05 12:59:25 +0100

    Add player death animation

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
 public int HP = 100;
-
+public GameObject bloodyScreen;
 public void TakeDamage(int damageAmount)
 {
 
@@ -18,12 +19,68 @@ int randomValue = Random.Range(0,2); // 0 or 1
 if (randomValue == 0)
 {
     print("Player Dead");
+    PlayerDead();
 }
-else{
+else
+{
     print("Player Hit");
+    StartCoroutine(BloodyScreenEffect());
 }
 }
 }
+
+private void PlayerDead()
+{
+    GetComponent<PlayerLook>().enabled = false;
+       GetComponent<PlayerMotor>().enabled = false;
+
+       // Dying animation
+       GetComponentInChildren<Animator>().enabled = true;
+}
+
+private IEnumerator BloodyScreenEffect()
+{
+    if (bloodyScreen.activeInHierarchy == false)
+    {
+        bloodyScreen.SetActive(true);
+    }
+
+  var image = bloodyScreen.GetComponentInChildren<Image>();
+ 
+        // Set the initial alpha value to 1 (fully visible).
+        Color startColor = image.color;
+        startColor.a = 1f;
+        image.color = startColor;
+ 
+        float duration = 3f;
+        float elapsedTime = 0f;
+ 
+        while (elapsedTime < duration)
+        {
+            // Calculate the new alpha value using Lerp.
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / duration);
+ 
+            // Update the color with the new alpha value.
+            Color newColor = image.color;
+            newColor.a = alpha;
+            image.color = newColor;
+ 
+            // Increment the elapsed time.
+            elapsedTime += Time.deltaTime;
+ 
+            yield return null; ; // Wait for the next frame.
+        }
+
+
+    if (bloodyScreen.activeInHierarchy)
+    {
+        bloodyScreen.SetActive(false);
+    }
+}
+
+
+
+
 private void OnTriggerEnter(Collider other)
 {
     if (other.CompareTag("ZombieHand"))
